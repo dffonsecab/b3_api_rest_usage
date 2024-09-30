af61c11db3081073609cce42a89568eee0cd3ad6
Diego Fernando <diegofernandofonseca@hotmail.com>
2024-09-30 15:37:59 -0500

    Practice

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import ApiCoffeCarruselBasic from './components/ApiCoffeCarruselBasic'
+
+function App() {
+
+
+  return (
+    <>
+     
+      
+        <ApiCoffeCarruselBasic/>
+
+  
+    
+    </>
+  )
+}
+
+export default App
